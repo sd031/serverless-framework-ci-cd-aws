b42d1f81999ef6fa372c02ca405bb5af940adbbb
Sandip Das <sandip@gryphonconnect.com>
2021-04-04 00:54:20 +0530

    v1 change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Serverless ci/cd v1!')
 })
 
 module.exports.handler = serverless(app);
